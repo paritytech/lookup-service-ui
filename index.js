19972f1000c6155c5d66b9844f8017486859fd01
Jannis R <mail@jannisr.de>
2016-12-13 15:48:06 +0100

    basic form, fetch data

--- index.js
+++ index.js
@@ -3,17 +3,39 @@
 const yo = require('yo-yo')
 const document = require('global/document')
 
-const render = (state, actions) => {
-	return yo `
-		<p>todo</p>
-	`
-}
+const _lookup = require('./lib/lookup')
+const util = require('./lib/util')
+const render = require('./lib/render')
 
 // state
-const state = {}
+const state = {
+	testnet: true,
+	input: 'jannis@ethcore.io'
+}
 
 // actions
-const actions = {}
+const setTestnet = (testnet) => {
+	state.testnet = testnet
+	rerender()
+}
+const setInput = (input) => {
+	state.input = input
+	rerender()
+}
+
+const lookup = () => {
+	const input = state.input
+	let req
+	if (util.isValidEmail(input)) req = _lookup({email: input})
+	else if (util.isValidAddress(input)) req = _lookup({address: input})
+	else return console.error('invalid input')
+}
+
+const actions = {
+	setTestnet: setTestnet,
+	setInput: setInput,
+	lookup: lookup
+}
 
 const root = render(state, actions)
 document.body.appendChild(root)
