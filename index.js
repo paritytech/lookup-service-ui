d5c94852dbb721e88087afbb274674317f233b4c
Jannis R <mail@jannisr.de>
2016-12-13 16:21:35 +0100

    linting :shirt:, ES2015

--- index.js
+++ index.js
@@ -9,53 +9,58 @@ const render = require('./lib/render')
 
 // state
 const state = {
-	testnet: true,
-	input: 'jannis@ethcore.io',
-	loading: false,
-	error: null,
-	data: null
+  testnet: true,
+  input: 'jannis@ethcore.io',
+  loading: false,
+  error: null,
+  data: null
 }
 
 // actions
 const setTestnet = (testnet) => {
-	state.testnet = testnet
-	rerender()
+  state.testnet = testnet
+  rerender()
 }
 const setInput = (input) => {
-	state.input = input
-	rerender()
+  state.input = input
+  rerender()
 }
 
 const lookup = () => {
-	const input = state.input
-	let req
-	if (util.isValidEmail(input)) req = _lookup({email: input})
-	else if (util.isValidAddress(input)) req = _lookup({address: input})
-	else return state.error = 'invalid input'
+  const input = state.input
+  let req
+  if (util.isValidEmail(input)) {
+    req = _lookup({email: input})
+  } else if (util.isValidAddress(input)) {
+    req = _lookup({address: input})
+  } else {
+    state.error = 'invalid input'
+    return
+  }
 
-	state.loading = true
-	req
-	.then((data) => {
-		state.data = data
-		state.loading = false
-		rerender()
-	})
-	.catch((err) => {
-		state.error = err.message
-		state.data = null
-		state.loading = false
-		rerender()
-	})
+  state.loading = true
+  req
+  .then((data) => {
+    state.data = data
+    state.loading = false
+    rerender()
+  })
+  .catch((err) => {
+    state.error = err.message
+    state.data = null
+    state.loading = false
+    rerender()
+  })
 }
 
 const actions = {
-	setTestnet: setTestnet,
-	setInput: setInput,
-	lookup: lookup
+  setTestnet: setTestnet,
+  setInput: setInput,
+  lookup: lookup
 }
 
 const root = render(state, actions)
 document.body.appendChild(root)
 const rerender = () => {
-	yo.update(root, render(state, actions))
+  yo.update(root, render(state, actions))
 }
